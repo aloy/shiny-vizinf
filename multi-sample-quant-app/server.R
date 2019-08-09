eb8ffca0768da03ba50b8275cdd61715ab65e105
Adam Loy <loyad01@gmail.com>
2019-08-09 12:21:18 -0500

    cleaning up reactive programming

--- multi-sample-quant-app/server.R
+++ multi-sample-quant-app/server.R
@@ -8,6 +8,16 @@ library(infer)    # tools for simulation-based inference
 library(Lock5Data)
 
 shinyServer(function(input, output, session){
+  
+  rv <- reactiveValues(show = FALSE)
+  
+  observeEvent(input$goButton, {
+    rv$show <- TRUE
+  })
+  
+  observeEvent(input$inputData, {
+    rv$show <- FALSE
+  })
 
   shinyjs::onclick("hideDataOptions",
                    shinyjs::toggle(id = "dataOptions", anim = TRUE))
@@ -15,22 +25,28 @@ shinyServer(function(input, output, session){
   
   theData <- reactive({
     if(input$inputData == "Upload data") {
-      
-      inFile1 <- input$file1
-      if (is.null(inFile1)) return(NULL)
-      
-      return(read.csv(inFile1$datapath, header=input$header, sep=input$sep, quote=input$quote))
+      req(input$file1)
+      return(read.csv(input$file1$datapath, header = input$header, sep = input$sep, quote = input$quote))
     } else {
       return(get(input$inputData))
     }
   })
 
+  
+  variables <- reactive({
+    req(theData())
+    list(
+      cat   = colnames(theData())[sapply(theData(), is.factor)],
+      quant = colnames(theData())[sapply(theData(), is.numeric)]
+    )
+  })
+  
 observe({
-  data <- theData()
-  cvars <- colnames(data)[sapply(data,is.factor)]
-  qvars <- colnames(data)[sapply(data,is.numeric)]
-  updateSelectInput(session, 'group', choices = cvars)
-  updateSelectInput(session, 'response', choices = qvars)
+  # data <- theData()
+  # cvars <- colnames(data)[sapply(data,is.factor)]
+  # qvars <- colnames(data)[sapply(data,is.numeric)]
+  updateSelectInput(session, 'group', choices = variables()$cat)
+  updateSelectInput(session, 'response', choices = variables()$quant)
 })  
 
 
@@ -40,40 +56,37 @@ output$theData <- renderDataTable(theData(),
 
 
 filteredData <- reactive({
-  data <- isolate(theData())
-  #if there is no input, make a dummy dataframe
-  if(input$group == "group" && input$response == "response"){
-    if(is.null(data)){
-      data <- data.frame(group = 0, response = 0)
-    }
-  }else{
-    data <- data[,c(input$group,input$response)]
-    names(data) <- c("group","response")
-  }
-  data
+  req(theData())
+  
+  data <- theData()[, c(input$group, input$response)]
+  names(data) <- c("group", "response")
+
+  data  
 })
 
 output$origPlot <- renderPlot({
   dataPlot <- switch(input$plot,
                      box = ggplot(filteredData(), aes(x = group, y = response)) +
-                       geom_boxplot(aes(fill = group, color = group), alpha = 0.6) +
+                       geom_boxplot(aes(fill = group, color = group), alpha = 0.4) +
                        geom_point(aes(x = group, y = mean, fill = group, color = group), 
-                                  data = filteredData() %>% group_by(group) %>% summarize(mean = mean(response, na.rm = TRUE))) +
-                       scale_fill_colorblind() +
-                       scale_color_colorblind(),
+                                  data = filteredData() %>% 
+                                    group_by(group) %>% 
+                                    summarize(mean = mean(response, na.rm = TRUE))
+                                  ),
                      den = ggplot(filteredData(), aes(x = response)) +
-                       geom_density(aes(fill = group, color = group), alpha = 0.6) +
-                       scale_fill_colorblind() +
-                       scale_color_colorblind(),
+                       geom_density(aes(fill = group, color = group), alpha = 0.4),
                      violin = ggplot(filteredData(), aes(x = group, y = response)) +
-                       geom_violin(aes(fill = group, color = group), alpha = 0.6) +
+                       geom_violin(aes(fill = group, color = group), alpha = 0.4) +
                        geom_point(aes(x = group, y = mean, fill = group, color = group), 
-                                  data = filteredData() %>% group_by(group) %>% summarize(mean = mean(response, na.rm = TRUE))) +
-                       scale_fill_colorblind() +
-                       scale_color_colorblind()
+                                  data = filteredData() %>% 
+                                    group_by(group) %>% 
+                                    summarize(mean = mean(response, na.rm = TRUE)))
                     
   )
-  dataPlot
+  dataPlot +
+    scale_fill_colorblind() +
+    scale_color_colorblind() +
+    theme_bw()
 })
 
 
@@ -84,15 +97,15 @@ output$basicSummary <- renderTable({
 
 
 lineupData <- reactive({
-  if(input$goButton > 0) {
-      filteredData() %>%
-      specify(response ~ group) %>% 
-      hypothesize(null = "independence") %>%
-      generate(reps = input$num - 1, type = "permute") %>%
-      as_tibble() %>%
-      bind_rows(filteredData()) %>%
-      mutate(.id = sample(input$num, size = input$num, replace = FALSE) %>% rep(each = nrow(filteredData())))
-  }
+  req(input$goButton)
+  N <- isolate(input$num)
+  filteredData() %>%
+    specify(response ~ group) %>% 
+    hypothesize(null = "independence") %>%
+    generate(reps = N - 1, type = "permute") %>%
+    as_tibble() %>%
+    bind_rows(filteredData()) %>%
+    mutate(.id = sample(N, size = N, replace = FALSE) %>% rep(each = nrow(filteredData())))
 })
 
 groupMeans <- reactive({
@@ -103,32 +116,30 @@ groupMeans <- reactive({
 
 
 output$lineup <- renderPlot({
-  if(input$goButton > 0) {
-    input$goButton
-    lineup_type <- isolate(input$lineup)
-    lineup_cols <- isolate(input$ncols)
-    lineup_plot <- switch(lineup_type,
-                          box = lineupData() %>%
-                            # as.data.frame() %>%
-                            ggplot() +
-                            geom_boxplot(aes(x = group, y = response, fill = group, color = group), alpha = 0.6) +
-                            geom_point(aes(x = group, y = mean, fill = group, color = group), 
-                                       data = groupMeans()),
-                          den = lineupData() %>%
-                            ggplot() +
-                            geom_density(aes(x = response, fill = group, color = group), alpha = 0.6),
-                          violin = lineupData() %>%
-                            ggplot() +
-                            geom_violin(aes(x = group, y = response, fill = group, color = group), alpha = 0.6) +
-                            geom_point(aes(x = group, y = mean, fill = group, color = group), 
-                                       data = groupMeans())
-    )
-    lineup_plot +
-      facet_wrap(~.id, ncol = lineup_cols) +
-      scale_fill_colorblind() +
-      scale_color_colorblind() +
-      theme(legend.position = "none")
-  }
+  req(rv$show)
+  lineup_type <- input$lineup
+  lineup_cols <- isolate(input$ncols)
+  lineup_plot <- switch(lineup_type,
+                        box = lineupData() %>%
+                          ggplot() +
+                          geom_boxplot(aes(x = group, y = response, fill = group, color = group), alpha = 0.6) +
+                          geom_point(aes(x = group, y = mean, fill = group, color = group), 
+                                     data = groupMeans()),
+                        den = lineupData() %>%
+                          ggplot() +
+                          geom_density(aes(x = response, fill = group, color = group), alpha = 0.6),
+                        violin = lineupData() %>%
+                          ggplot() +
+                          geom_violin(aes(x = group, y = response, fill = group, color = group), alpha = 0.6) +
+                          geom_point(aes(x = group, y = mean, fill = group, color = group), 
+                                     data = groupMeans())
+  )
+  lineup_plot +
+    facet_wrap(~.id, ncol = lineup_cols) +
+    scale_fill_colorblind() +
+    scale_color_colorblind() +
+    theme_minimal() +
+    theme(legend.position = "none") 
 },
 height = function() {
   0.8 * session$clientData$output_lineup_width
