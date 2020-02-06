57e876c1637e59feb541345d0eb80cb0d50aeeba
Adam Loy <loyad01@gmail.com>
2020-02-06 10:04:08 -0600

    about tab info

--- lm-resids-app/ui.R
+++ lm-resids-app/ui.R
@@ -139,11 +139,65 @@ navbarPage(
 
   tabPanel(
     "About",
-    h3("Comparing groups vis the lineup protocol"),
-    p("This Shiny app is intended..."),
+    h3("App overview"),
+    p(
+      "This app allows you to render lineups of residual plots 
+      for simple linear regression models. This can help you learn 
+      to interpret residual plots, as it hones your intuition of 
+      what signal and noise mean in a residual plot. To use the app:",
+      tags$ul(
+        tags$li("Choose a data set or upload your own."),
+        tags$li("Specify the response and explanatory variables."),
+        tags$li("Generate a lineup of residual plots. You can render the 
+        residuals vs. the fitted values or the explanatory variable, or 
+                you can render a normal Q-Q plot of the standardized residuals."),
+        tags$li("Inspect the lineups and reveal the data plot once you have made your selection.")
+      ),
+      "After inspecting the lineup, you can focus on the observed data in the data plot tab."
+    ),
+    h3("Learning goals"),
+    p(
+      "This app is intended to help students build their intuition about residual plots.
+       Instead of showing students a single residual plot and talking about 'random scatter' or 
+       'patterns,' having students identify the 'most different' plot and discuss why they
+       made their choice will help students figure out what type of signal is problematic.
+       This is facilitated by they fact that lineups force you to compare the observed plot
+       to plots taken from the distribution of noise plots (i.e. plots generated from an
+       appropriate model)."
+    ),
+    h3("Example class usage"),
+    p(
+      "I recommend building a guided activity for your students.",
+      tags$ol(
+        tags$li("Introduce the data set and problem."),
+        tags$li("Ask students to plot the data and fit the regression model.
+                Depending on your goals, you can have them interpret the coefficients, etc."),
+        tags$li("Instead of having students construct a single residual plot, have students
+        generate a lineup of residual plots. Quickly explain that one panel displays
+                the observed residual plot while the other panels are decoy plots generated
+                under the assumption that the model fits (i.e. not assumptions were violated).
+                Have students choose the plot that is most different and justify their 
+                answer before revealing the answer."),
+        tags$li("Ask students to discuss their decision in light of the 'answer' and
+               what that indicates about the appropriateness of the model.") 
+      )
+    ),
+    h3("Additional resources"),
+    p("Below are additional resources to help you learn about visual inference and how it can be used in 
+      the classroom:",
+      tags$ul(
+        tags$li(a("Tutorial on creating lineups in R using the nullabor package", 
+                  href="https://aloy.github.io/classroom-vizinf/")),
+        tags$li(a("Introductory paper on visual inference (Buja et al., 2009)", href="https://royalsocietypublishing.org/doi/abs/10.1098/rsta.2009.0120"))
+      )
+    ),
     h3("Author"),
-    p("Adam Loy"),
-    p("Creative commons license...")
+    p("Adam Loy - ", a("aloy.rbind.io", href = "https://aloy.rbind.io")),
+    a(img(src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"), rel="license", 
+      href="http://creativecommons.org/licenses/by-nc-sa/4.0/"),
+    p("This work is licensed under a", 
+      a("Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.",  
+        href="http://creativecommons.org/licenses/by-nc-sa/4.0/"))
   )
   
   
